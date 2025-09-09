7846bdcc112e2195e5d3714ef2f8fdadb0d06640
Ankush11verma <161729157+Ankush11verma@users.noreply.github.com>
2025-09-09 20:18:32 +0530

    Added new feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./App.css";
 import Header from "./header";
 import Transaction from "./Transaction";
-import Table from "./table";
+import Table from "./Table";
 import IncomeExpenseChart from "./IncomeExpenseChart";
 
 function App() {
